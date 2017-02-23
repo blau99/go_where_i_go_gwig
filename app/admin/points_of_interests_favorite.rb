cda7f61439f5d11259bf06875e9319be27c86b53
Raghu Betina <raghu@firstdraft.com>
2017-02-23 06:01:55 +0000

    Whitelist columns in PointsOfInterestsFavorite for ActiveAdmin

--- app/admin/points_of_interests_favorite.rb
+++ app/admin/points_of_interests_favorite.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register PointsOfInterestsFavorite do
 
+ permit_params :points_of_interest_id, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
