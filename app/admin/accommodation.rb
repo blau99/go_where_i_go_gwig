dee986273dfbedfd9070c9382ab7f79ee222f492
Raghu Betina <raghu@firstdraft.com>
2017-02-23 06:01:19 +0000

    Whitelist columns in Accommodation for ActiveAdmin

--- app/admin/accommodation.rb
+++ app/admin/accommodation.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Accommodation do
 
+ permit_params :name, :address, :image, :cost_per_night, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
