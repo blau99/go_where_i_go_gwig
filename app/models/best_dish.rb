ec3053e0c0f7c701a3fa06d414fd84a88f6316ea
Raghu Betina <raghu@firstdraft.com>
2017-02-23 06:00:32 +0000

    Direct association between restaurants and best_dishes

--- app/models/best_dish.rb
+++ app/models/best_dish.rb
@@ -1,6 +1,8 @@
 class BestDish < ApplicationRecord
   # Direct associations
 
+  belongs_to :restaurant
+
   # Indirect associations
 
   # Validations
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,9 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  has_many   :best_dishes,
+             :dependent => :destroy
+
   belongs_to :user
 
   # Indirect associations
