3d5d4f609b395fc4df79e477080dc934f0899cb3
Raghu Betina <raghu@firstdraft.com>
2017-02-23 06:00:32 +0000

    Direct association between points_of_interests and points_of_interests_favorites

--- app/models/points_of_interest.rb
+++ app/models/points_of_interest.rb
@@ -1,6 +1,9 @@
 class PointsOfInterest < ApplicationRecord
   # Direct associations
 
+  has_many   :points_of_interests_favorites,
+             :dependent => :destroy
+
   has_many   :points_of_interests_photos,
              :dependent => :destroy
 
--- app/models/points_of_interests_favorite.rb
+++ app/models/points_of_interests_favorite.rb
@@ -1,6 +1,8 @@
 class PointsOfInterestsFavorite < ApplicationRecord
   # Direct associations
 
+  belongs_to :points_of_interest
+
   belongs_to :user
 
   # Indirect associations
