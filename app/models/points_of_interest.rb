c7adc74936b6a723ff86df18a848085594f8cba6
Raghu Betina <raghu@firstdraft.com>
2017-02-23 06:00:32 +0000

    Direct association between points_of_interests and points_of_interests_photos

--- app/models/points_of_interest.rb
+++ app/models/points_of_interest.rb
@@ -1,6 +1,9 @@
 class PointsOfInterest < ApplicationRecord
   # Direct associations
 
+  has_many   :points_of_interests_photos,
+             :dependent => :destroy
+
   belongs_to :user
 
   # Indirect associations
