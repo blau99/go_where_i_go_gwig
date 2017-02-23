aab5173e53b5fbd2e418ce1a926dca635a57fd48
Raghu Betina <raghu@firstdraft.com>
2017-02-23 06:00:32 +0000

    Direct association between users and points_of_interests

--- app/models/points_of_interest.rb
+++ app/models/points_of_interest.rb
@@ -1,6 +1,8 @@
 class PointsOfInterest < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :points_of_interests,
+             :dependent => :destroy
+
   has_many   :accommodations,
              :dependent => :destroy
 
