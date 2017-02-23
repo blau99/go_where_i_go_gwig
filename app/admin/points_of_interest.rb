309048055822911ffc7ae46ecbfd2b2e7e46b8f0
Raghu Betina <raghu@firstdraft.com>
2017-02-23 06:01:14 +0000

    Whitelist columns in PointsOfInterest for ActiveAdmin

--- app/admin/points_of_interest.rb
+++ app/admin/points_of_interest.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register PointsOfInterest do
 
+ permit_params :name, :address, :admission_fee, :image, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
